b6f807cc1ad1b45636ab59b76d1f3663836593a3
shadi <shadi@shadi-gl553ve.lan>
2022-08-27 19:07:25 +0430

    producer.sh: edit comment

--- kafka_script/producer.sh
+++ kafka_script/producer.sh
@@ -5,14 +5,12 @@ kafka-topics.sh --create --if-not-exists --topic cpu_time --partitions 1 --repli
 
 filename='/opt/bitnami/kafka_data/cpu_usage.data'
 n=1
+
 while read line; do
 # reading each line
 echo "Line No. $n : $line"
-echo "*************"
-# send 
 echo "$line" | kafka-console-producer.sh --broker-list kafka:9093 --topic cpu_time
 n=$((n+1))
-sleep 5
 done < $filename
 
 
